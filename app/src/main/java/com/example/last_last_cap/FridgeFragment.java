766075cfdc97ede085eb0119d130e47ca599d644
gi0-h <hwicksky2@naver.com>
2023-08-15 21:50:47 +0900

    8/15

--- app/src/main/java/com/example/last_last_cap/FridgeFragment.java
+++ app/src/main/java/com/example/last_last_cap/FridgeFragment.java
@@ -1,53 +1,93 @@
 package com.example.last_last_cap;
 
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
-import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
 public class FridgeFragment extends Fragment {
 
-    public FridgeFragment() {
-        // Required empty public constructor
-    }
+    private LinearLayout buttonContainer;
+    private List<Button> buttons;
+
+    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_fridge, container, false);
 
-        Button calander = view.findViewById(R.id.calander_button);
-        calander.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showCalendarDialog();
-            }
-        });
+        buttonContainer = view.findViewById(R.id.buttonContainer);
+        buttons = new ArrayList<>();
+
+        Button addButton = view.findViewById(R.id.addButton);
+        addButton.setOnClickListener(v -> showAddDialog());
+
+        Button calendarButton = view.findViewById(R.id.calendarButton);
+        calendarButton.setOnClickListener(v -> showCalendarDialog());
 
         return view;
     }
+
+    private void showAddDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Add Button");
+
+        View dialogView = LayoutInflater.from(getActivity()).inflate(R.layout.dialog_add_button, null);
+        builder.setView(dialogView);
+
+        final EditText nameEditText = dialogView.findViewById(R.id.nameEditText);
+
+        builder.setPositiveButton("Add", (dialog, which) -> {
+            String name = nameEditText.getText().toString();
+            addButtonWithName(name);
+        });
+
+        builder.setNegativeButton("Cancel", null);
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    private void addButtonWithName(String name) {
+        Button button = new Button(getActivity());
+        button.setText(name);
+
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+
+        button.setLayoutParams(layoutParams);
+        buttons.add(button);
+
+        buttonContainer.addView(button);
+    }
+
     private void showCalendarDialog() {
-        // Get current date
         final Calendar calendar = Calendar.getInstance();
         int year = calendar.get(Calendar.YEAR);
         int month = calendar.get(Calendar.MONTH);
         int day = calendar.get(Calendar.DAY_OF_MONTH);
 
-        // Create DatePickerDialog
         DatePickerDialog datePickerDialog = new DatePickerDialog(getContext(),
-                new DatePickerDialog.OnDateSetListener() {
-                    @Override
-                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-                        // Handle selected date
-                        String selectedDate = year + "-" + (monthOfYear + 1) + "-" + dayOfMonth;
-                        Toast.makeText(getContext(), selectedDate, Toast.LENGTH_SHORT).show();
-                    }
+                (view, year1, monthOfYear, dayOfMonth) -> {
+                    String selectedDate = year1 + "-" + (monthOfYear + 1) + "-" + dayOfMonth;
+                    Toast.makeText(getContext(), selectedDate, Toast.LENGTH_SHORT).show();
                 }, year, month, day) {
             @Override
             public void onDateChanged(DatePicker view, int year, int month, int dayOfMonth) {
@@ -60,7 +100,9 @@ public class FridgeFragment extends Fragment {
             }
         };
 
-        // Show DatePickerDialog
+        datePickerDialog.getDatePicker().setMinDate(calendar.getTimeInMillis());
+        datePickerDialog.getDatePicker().setMaxDate(calendar.getTimeInMillis());
+
         datePickerDialog.show();
     }
 
@@ -71,7 +113,4 @@ public class FridgeFragment extends Fragment {
         calendar.set(Calendar.DAY_OF_MONTH, picker.getDayOfMonth());
         return calendar.getTimeInMillis();
     }
-
-
-
 }
